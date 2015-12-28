a8a77a5efb871269e1c1b5cbbd50e3349036b503
Castlebin <castlebin@C-L-PC>
2015-12-28 15:52:27 +0800

    something

--- linux-shell/shell_template_utf8.sh
+++ linux-shell/shell_template_utf8.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# filename: echo.sh
+# filename: shell_template_utf8.sh
 
 echo "Hello World!"
-echo "Hello World\!"
-echo -e "Hello World\!，我爱夏天！"
+
+echo "Hello World\t，我爱夏天！"
+echo -e "Hello World\t，我爱夏天！"
 
 echo -e "\e[1;31m This is red text! \e[0m"
 echo -e "\e[1;42m Green Background \e[0m"
@@ -15,5 +16,3 @@ echo "We have $number $fruit(s)"
 length=${#fruit}    # shell脚本中变量用大括号括起来{}
 echo $length    # shell脚本中变量取值用$符号
 echo ${#fruit}  # #var 是var的一个特殊变量，代表var变量的长度
-
-
