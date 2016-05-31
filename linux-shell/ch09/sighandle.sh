1c56700265e128a22af8daa76d599b3cd5c8fe50
Castlebin <Castlebin@gmail.com>
2016-05-31 15:05:03 +0800

    扑捉并相应信号

--- linux-shell/ch09/sighandle.sh
+++ linux-shell/ch09/sighandle.sh
@@ -2,15 +2,14 @@
 # filename: sighandle.sh
 
 function handler() {
-	echo "Hey, received signal: SIGINT."
+    echo "Hey, received signal: SIGINT."
 }
 
 echo "My Process ID is $$"
 
-trap 'handler' SIGINT	# 本脚本中将handler函数和SIGINT信号量绑定在一起
+trap 'handler' SIGINT   # 本脚本中将handler函数和SIGINT信号量绑定在一起
 
 while true; do
-	sleep 1
+    sleep 1
 done
-
 
