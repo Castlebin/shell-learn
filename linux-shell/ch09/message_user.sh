1944f93078ae863ab0ef56b36176e14fe836ca68
Castlebin <Castlebin@gmail.com>
2016-05-31 15:50:08 +0800

    向指定用户终端发送消息

---
+++ linux-shell/ch09/message_user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# filename: message_user.sh
+# 用途：向指定用户的终端发送消息
+
+USER=$1
+devices=`ls /dev/pts/* -l | awk '{ print $3,$10 }' | grep $USER | awk '{ print $2 }'`
+
+echo devices:
+echo $devices;
+
+for dev in $devices
+do
+	cat /dev/stdin > $dev
+done
+
